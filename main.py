17fcc5a1335705aa32041b2fc6038d2c533af9b7
riccardo <31276674+xRowie@users.noreply.github.com>
2022-12-28 14:46:57 +0100

    add unet block

---
+++ main.py
@@ -0,0 +1,8 @@
+import torch
+
+z = torch.Tensor([[1,2,3,4], [5,6,7,8]])
+
+print(z)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f'Selected device: {device}')
---
+++ utils/encoder_block.py
@@ -0,0 +1,15 @@
+from torch import nn
+from utils import conv_block
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super.__init__()
+        self.conv = conv_block.ConvBlock(in_channels, out_channels, kernel_size, stride, padding)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.pool(x)
+        return x
+
